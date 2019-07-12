2c37d69a32906f6311195c9ebaccb37977320e23
WUPA2 <panda.wu@oocl.com>
2019-07-12 15:27:54 +0800

    convert function add 3 multiply

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String convert(int num){
-        if (num==3) return "Fizz";
+        if (num%3==0) return "Fizz";
         if (num==5) return "Buzz";
         return String.valueOf(num);
     }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -43,4 +43,14 @@ public class FizzBuzzTest {
         //Then
         Assertions.assertEquals("Buzz",convert);
     }
+    @Test
+    public void return_Fizz_when_input_6(){
+        //Given
+        int num=6;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //When
+        String convert = fizzBuzz.convert(num);
+        //Then
+        Assertions.assertEquals("Fizz",convert);
+    }
 }
